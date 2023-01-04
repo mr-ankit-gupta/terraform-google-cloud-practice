8252bfbb81f373637dc8aaf60a4563235fc251f9
mr-ankit-gupta <ankitr985@gmail.com>
2023-01-04 11:05:52 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.85.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  project = "qwiklabs-gcp-00-616a397f0933"
+  region = "asia-south1"
+  zone = "asia-south1-a"
+}
+
+resource "google_storage_bucket" "my-bckt" {
+  
+  name = "my-first-tf-bucket"
+  storage_class = "NEARLINE"
+  location = "US-CENTRAL1"
+  uniform_bucket_level_access = true
+}
